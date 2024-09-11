4498357d125138791c98b9ad480da1ae5e4e8459
Justice Calderon <just.cald4@gmail.com>
2024-09-11 17:15:09 -0600

    Fixing time embedding code

--- src/unet.py
+++ src/unet.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import torch
 from torch import nn
 from convolution import ConvolutionalNeuralNetwork_2D
 import matplotlib.pyplot as plt
@@ -10,13 +11,21 @@ class U_Net_Convolution(nn.Module):
 
     @staticmethod
     def time_position_embedd(time_steps, time_embed_dim):
-        denom_fact = 10000 ** (np.arange(start=0, stop=(time_embed_dim//2)) / (time_embed_dim // 2))
+        denom_fact = 10000 ** (torch.arange(start=0, end=(time_embed_dim//2)) / (time_embed_dim // 2))
         time_embedding = time_steps[:, None].repeat(time_embed_dim//2, 1) / denom_fact
 
         print(f"Denominator Shape: {denom_fact.shape}\n")
-
-        time_embedding = np.concatenate((np.sin(time_embedding), np.cos(time_embedding)), axis=1)
         print(f"Time Embedding Shape: {time_embedding.shape}")
+
+        output = torch.zeros(len(time_steps), time_embed_dim)
+        output[:, ::2] = torch.sin(time_embedding)
+        output[:, 1::2] = torch.cos(time_embedding)
+
+        # fig = plt.figure()
+        # ax1 = fig.add_subplot(111)
+        # ax1.imshow(output)
+        # ax1.set_aspect('auto')
+        # fig.savefig('equal.png')
 
         return time_embedding
     
@@ -55,11 +64,13 @@ class ResNet_SelfAttention(nn.Module):
     
 
 if __name__ == "__main__":
-    l = np.random.rand(3, 64, 64)
-    a = ConvolutionalNeuralNetwork_2D(3, 64, (3, 3), 1, padding_type='valid')
-    a.convlutional_activation_layer(a.weights.shape[0], a.weights.shape[0])
-    output = a(l)
-    output = output.sum()
-    output.backward()
-    print(f"dO / dW:\n{a.weights.grad}\ndO / dB:\n{a.bias.grad}")
-    
+    # l = np.random.rand(3, 64, 64)
+    # a = ConvolutionalNeuralNetwork_2D(3, 64, (3, 3), 1, padding_type='valid')
+    # a.convlutional_activation_layer(a.weights.shape[0], a.weights.shape[0])
+    # output = a(l)
+    # output = output.sum()
+    # output.backward()
+    # print(f"dO / dW:\n{a.weights.grad}\ndO / dB:\n{a.bias.grad}")
+    # time_steps = np.linspace(start=1e-4, stop=0.002, num=1000)
+    # time_embedding = U_Net_Convolution.time_position_embedd(time_steps=np.array(range(0, len(time_steps))), time_embed_dim=32)
+    pass
